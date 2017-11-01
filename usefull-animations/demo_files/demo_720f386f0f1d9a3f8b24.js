3f3ded3310c14aa495c9a6ba165d4b29ca50e719
Clarence <clarence-pan@foxmail.com>
2017-11-01 15:18:45 +0800

    auto-build-pages

---
+++ usefull-animations/demo_files/demo_720f386f0f1d9a3f8b24.js
@@ -0,0 +1,2 @@
+!function(t){function s(a){if(r[a])return r[a].exports;var c=r[a]={exports:{},id:a,loaded:!1};return t[a].call(c.exports,c,c.exports,s),c.loaded=!0,c.exports}var r={};return s.m=t,s.c=r,s.p="",s(0)}([function(t,s){"use strict";var r=$(".test");$(".toolbar").on("click","a",function(t){var s=$(t.currentTarget);s.is(".stop")?r.attr("class","test"):(r.attr("class","test"),r.css("opacity"),r.css("width"),r.css("top"),r.addClass(s.data("class")))})}]);
+//# sourceMappingURL=demo_720f386f0f1d9a3f8b24.js.map
